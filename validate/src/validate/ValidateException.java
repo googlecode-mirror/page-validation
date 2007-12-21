e1bf2cd93a0866b1b22faa864d333bd634bb7e6f
niebo2006 <niebo2006@f68bd8b5-9041-0410-80e2-f1163fee2ef0>
2007-12-21 16:13:45 +0000

    start page-validation project 开始页面校验项目

---
+++ validate/src/validate/ValidateException.java
@@ -0,0 +1,56 @@
+﻿/*
+ * 创建日期 2007-12-14
+ *
+ * TODO 要更改此生成的文件的模板，请转至
+ * 窗口 － 首选项 － Java － 代码样式 － 代码模板
+ */
+package validate;
+
+
+/**
+ * @author kfzx-niebo
+ *
+ * TODO 要更改此生成的类型注释的模板，请转至
+ * 窗口 － 首选项 － Java － 代码样式 － 代码模板
+ */
+public class ValidateException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8220695458773277968L;
+
+	/**
+	 * 
+	 */
+	public ValidateException() {
+		super();
+		// TODO 自动生成构造函数存根
+	}
+
+	/**
+	 * @param message
+	 */
+	public ValidateException(String message) {
+		super(message);
+		// TODO 自动生成构造函数存根
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public ValidateException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO 自动生成构造函数存根
+	}
+
+	/**
+	 * @param cause
+	 */
+	public ValidateException(Throwable cause) {
+		super(cause);
+		// TODO 自动生成构造函数存根
+	}
+
+}
