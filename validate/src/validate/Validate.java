e04756e5c0d233e3f9125072b8f8231f81dd53e2
niebo2006 <niebo2006@f68bd8b5-9041-0410-80e2-f1163fee2ef0>
2007-12-22 07:25:43 +0000

    更改接口调用方式

--- validate/src/validate/Validate.java
+++ validate/src/validate/Validate.java
@@ -1,8 +1,7 @@
 ﻿/*
  * 创建日期 2007-12-14
- * 
- * TODO 要更改此生成的文件的模板，请转至 窗口 － 首选项 － Java － 代码样式 － 代码模板
- */
+ * js验证函数的java接口
+ * */
 package validate;
 
 import java.io.IOException;
@@ -12,18 +11,10 @@ import java.util.List;
 
 import javax.servlet.jsp.JspWriter;
 
-import validate.htmField.FloatField;
-import validate.htmField.GeneralField;
 import validate.htmField.ValHtmField;
 
 /**
- * @author kfzx-niebo
- * 
- * TODO 要更改此生成的类型注释的模板，请转至 窗口 － 首选项 － Java － 代码样式 － 代码模板
- */
-/**
- * @author niebo
- *
+ * @author kfzx-niebo js验证函数的java接口
  */
 public class Validate {
 
@@ -32,84 +23,30 @@ public class Validate {
 	private String subCheckFormId = null;
 
 	/**
-	 * 添加一个需要校验的Float场，该方法写入js同名方法调用到网页以
-	 * @param htmFieldID Float场的ID属性
-	 * @param htmTitle  提示信息的标题
-	 * @param htmMaxLen 该场的maxlength属性
-	 * @param htmFieldSize 该场得size属性
-	 * @param canEmpty  该场是否允许提交空值
-	 * @param intNum 该场的整数部分最大长度
-	 * @param decNum 该场的小数部分最大长度
-	 * @param maxValStr 该场的最大值 可输入null表示无上限
-	 * @param minValStr 该场的最小值 可输入null表示无下限
-	 * @param custCheckFun 需要调用的特殊js校验方法名 在基本校验结束时调用 可以输入“null”表示无特殊教研
+	 * @param htmField
+	 *            htmField的子类。
 	 */
-	public void addFloatField(String htmFieldID, String htmTitle, int htmMaxLen, int htmFieldSize, boolean canEmpty,
-			int intNum, int decNum, String maxValStr, String minValStr, String custCheckFun) {
-		FloatField floatField = new FloatField(htmFieldID,htmTitle,htmMaxLen,htmFieldSize,canEmpty,intNum,decNum,maxValStr,minValStr,custCheckFun);
-		htmFieldList.add(floatField);
+	public void addHtmField(ValHtmField htmField) {
+		htmFieldList.add(htmField);
 	}
 
 	/**
-	 * @see Validate#addFloatField(String, String, int, int, boolean, int, int, String, String, String)
-	 * @param htmFieldIDArr  Float场的ID属性数组
-	 * @param htmTitleArr 提示信息的标题数组
-	 * @param htmMaxLen 该场的maxlength属性
-	 * @param htmFieldSize 该场得size属性
-	 * @param canEmpty  该场是否允许提交空值
-	 * @param intNum 该场的整数部分最大长度
-	 * @param decNum 该场的小数部分最大长度
-	 * @param maxValStr 该场的最大值 可输入null表示无上限
-	 * @param minValStr 该场的最小值 可输入null表示无下限
-	 * @param custCheckFun 需要调用的特殊js校验方法名 在基本校验结束时调用 可以输入“null”表示无特殊教研
-	 * @throws ValidateException 
+	 * @param htmFields
+	 *            htmField的数组
 	 */
-	public void addFloatField(String[] htmFieldIDArr, String[] htmTitleArr, int htmMaxLen, int htmFieldSize,
-			boolean canEmpty, int intNum, int decNum, String maxValStr, String minValStr, String custCheckFun) throws ValidateException {
-		if(htmFieldIDArr.length != htmTitleArr.length) throw new ValidateException("length of htmFieldIDArr and htmTitleArr must equal!");
-		for (int i = 0; i < htmFieldIDArr.length; i++) {
-			addFloatField(htmFieldIDArr[i], htmTitleArr[i], htmMaxLen, htmFieldSize, canEmpty, intNum, decNum, maxValStr,
-					minValStr, custCheckFun);
-		}
+	public void addHtmFields(ValHtmField[] htmFields) {
+		for (int i = 0; i < htmFields.length; i++)
+			htmFieldList.add(htmFields[i]);
 	}
 
-	/**
-	 * @param htmFieldID Float场的ID属性
-	 * @param htmTitle  提示信息的标题
-	 * @param htmMaxLen 该场的maxlength属性
-	 * @param htmFieldSize 该场得size属性
-	 * @param canEmpty  该场是否允许提交空值
-	 * @param regExp 该场满足的js正则表达式
-	 * @param custCheckFun 需要调用的特殊js校验方法名 在基本校验结束时调用 可以输入“null”表示无特殊教研
-	 */
-	public void addGeneralField(String htmFieldID,String htmTitle,int htmMaxLen,int htmFieldSize,boolean canEmpty,String regExp,String custCheckFun){
-		htmFieldList.add(new GeneralField(htmFieldID,htmTitle,htmMaxLen,htmFieldSize,canEmpty,regExp,custCheckFun));
-	}
-
-	/**
-	 * @param htmFieldIDArr Float场的ID属性 数组
-	 * @param htmTitleArr  提示信息的标题 数组
-	 * @param htmMaxLen 该场的maxlength属性
-	 * @param htmFieldSize 该场得size属性
-	 * @param canEmpty  该场是否允许提交空值
-	 * @param regExp 该场满足的js正则表达式
-	 * @param custCheckFun 需要调用的特殊js校验方法名 在基本校验结束时调用 可以输入“null”表示无特殊教研
-	 * @throws ValidateException 
-	 */
-	public void addGeneralField(String[] htmFieldIDArr,String[] htmTitleArr,int htmMaxLen,int htmFieldSize,boolean canEmpty,String regExp,String custCheckFun) throws ValidateException{
-		if(htmFieldIDArr.length != htmTitleArr.length) throw new ValidateException("length of htmFieldIDArr and htmTitleArr must equal!");
-		for(int i =0;i < htmFieldIDArr.length;i++)
-			htmFieldList.add(new GeneralField(htmFieldIDArr[i],htmTitleArr[i],htmMaxLen,htmFieldSize,canEmpty,regExp,custCheckFun));
-	}
-
-	
 	/**
 	 * @param out
 	 * @param formName
 	 * @throws IOException
 	 * @throws ValidateException
 	 */
-	public void writeJs(JspWriter out,String formId) throws IOException, ValidateException {
+	public void writeJs(JspWriter out, String formId) throws IOException,
+			ValidateException {
 		setSubCheckFormId(formId);
 		if (htmFieldList.size() > 0) {
 			out.println("<script language = \"javascript\" >");
@@ -122,7 +59,8 @@ public class Validate {
 				throw new ValidateException(
 						"you must call Validate.setSubCheckFormId first before call Validate.writeJs");
 			}
-			out.println("VALIDATION.setSubCheckForm(\"" + getSubCheckFormId() + "\");");
+			out.println("VALIDATION.setSubCheckForm(\"" + getSubCheckFormId()
+					+ "\");");
 			out.println("</script>");
 		}
 	}
